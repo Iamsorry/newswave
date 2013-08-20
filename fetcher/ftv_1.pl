724a19718139e468ecefa7d8ab11af21c750ad19
Iamsorry <code.project@m2k.com.tw>
2013-08-20 09:46:29 +0800

    Modify to use the portal page as title source instead of the news list provided by themselves.

--- fetcher/ftv_1.pl
+++ fetcher/ftv_1.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use LWP::UserAgent;
 
-my $URL = 'http://news.ftv.com.tw/NewsIframe.aspx';
+my $URL = 'http://news.ftv.com.tw/';
 
 my $ua = LWP::UserAgent->new;
 my $response = $ua->get($URL);
@@ -11,8 +11,7 @@ my @lines = split(/\n/, $response->decoded_content);
 
 foreach my $line (@lines)
 {
-	next unless $line =~ /<a href="(NewsContent.aspx[^"]+)" target="_parent">(.*)<\/a>/;
+	next unless $line =~ /<li><a href="(NewsContent\.aspx\?[^"]+)">(.*)<\/a><\/li>/;
 	my ($uri, $anchor) = ($1, $2);
-	$uri =~ s/&amp;/&/g;
 	printf "http://news.ftv.com.tw/%s\t%s\n", $uri, $anchor;
 }
